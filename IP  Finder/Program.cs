2f35b3c7a8170c50fc7831ef67ffa5a8e9857981
Brandon <31463964+BrandonJMorgan@users.noreply.github.com>
2018-06-20 18:47:19 -0500

    Added Code for IP finder
    This is a console application that finds the IP of any website or computer(Name) and displays the information.
    I used the .net frame with c# to create this

---
+++ IP  Finder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace IP__Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            string user = "Brandon";
+            int age = 21;
+            Console.Title = "IP Finder made by " + user + "\t age is \t " + age;
+            try
+            {
+                Console.WriteLine("Enter server name or domain name");
+                string name = Console.ReadLine();
+                IPHostEntry address = Dns.GetHostEntry(name);
+                IPAddress[] ip = address.AddressList;
+                string addrss = ip[0].ToString();
+                Console.WriteLine("the IP address for the website you entered is\t " + addrss);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
+
